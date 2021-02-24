610bfcbb6a9a598edbb93ad3cbaae2748c5fbe47
Gabriel <gabrielmg97@hotmail.com>
2021-02-24 10:00:49 -0300

    pequenas mudanças

--- ProgamasPython/Exercicio.py
+++ ProgamasPython/Exercicio.py
@@ -1,8 +1,9 @@
-def main():
+def main(): # Teste para somar 2 numeros simples.
     a = 3
     b = 4
     soma = a + b
-    print("A soma de a + b eh igual a", soma)
+    
+    print("A soma de a + b \n eh igual a", soma)
 
 main()
    
