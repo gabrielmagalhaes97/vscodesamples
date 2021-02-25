e659447bc3bda7880a22cc0f08f229f6a4d4d635
Gabriel <gabrielmg97@hotmail.com>
2021-02-25 16:34:45 -0300

    dgb

---
+++ exerciciosPython/aula2.py
@@ -0,0 +1,17 @@
+# EXERCICIO: Faça um formulario que pergunte
+# o nome,cpf, endereço,idade,altura e telefone 
+# e imprima isso em um relatorio organizado.
+
+nome = input("Escreva seu nome: ")
+cpf = input("Escreva seu CPF: ")
+endereco = input("Escreva seu Endereço: ")
+idade = input("Escreva sua idade: ")
+altura = input("Escreva sua altura: ")
+telefone = input("Escreva seu telefone: ") 
+
+print('\nNome:', nome)
+print('CPF:', cpf)
+print('EndereÃ§o:', endereco)
+print('Idade:', idade, 'anos')
+print('Altura:', altura, 'metros')
+print('Telefone:', telefone)
